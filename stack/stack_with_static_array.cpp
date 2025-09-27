b0622dd5912ca6d21cd5e67da5d2e17afc140b76
aryan-65548 <aryan.patel25jan@gmail.com>
2025-09-27 22:51:42 +0530

    Update stack_with_static_array.cpp
    made the changes here

--- stack/stack_with_static_array.cpp
+++ stack/stack_with_static_array.cpp
@@ -9,24 +9,27 @@ public:
     {
         top = -1;
     }
+
     void push(int x) //it will push the values into the stack
     {
-        if(top>= Max - 1)
+        if (top>= Max - 1)
         {
             cout << "stack overflow" << endl; //stack overflow happens when the stack is full
             return;
         }
          arr[++top] = x;
-    }
+     }
+
      void pop() //it will remove the the top of the stack value and will return it 
-    {
+     {
         if (top < 0) 
         {
             cout << "Stack Underflow\n"; //stack underflow happens when the stack is empty
             return;
         }
         top--;
-    }
+      }
+
     int peek() //it will return the top of the stack value but will not remove it 
     {
         if (top < 0) 
@@ -36,10 +39,12 @@ public:
         }
         return arr[top];
     }
+
      bool isEmpty()
     {
         return (top < 0);
     }
+
     void display()  // +it will display the stack
     {
         if (top < 0) 
@@ -58,6 +63,7 @@ public:
         }
     }
 }; 
+
 int main() {
     Stack s;
     s.push(10);
@@ -68,3 +74,4 @@ int main() {
     s.display();   
     return 0;
 }
+
